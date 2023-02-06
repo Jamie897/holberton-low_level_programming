6828821dd10dd604d3f09ee41ee86387b6cb4a69
Jamie897 <jamiedanielle456@gmail.com>
2023-02-05 19:14:23 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,18 +5,19 @@
  *
  * @str: arg *str
  *
- * Return: string to stdout
+ * Return: void
  */
 
 void puts2(char *str)
 {
-	int i;
+        int i; = 0;
 
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+        while (str[i] != '\0')
+        {
+                if (i % 2 == 0)
+                        _putchar(str[i]);
+                i++;
+        }
+        _putchar('\n');
 }
+
