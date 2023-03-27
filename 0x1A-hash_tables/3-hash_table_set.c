b68af0e0605d76e0db3952294cab32937304a5e2
Jamie897 <jamiedanielle456@gmail.com>
2023-03-26 17:58:25 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,82 +1,61 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * hash_table_set - set a value into a hash table
- *
- * @ht: Hash table
- * @key: Key
- * @value: Value
- *
- * Return: 1 - success or 0 - failure
-*/
-
+ * hash_table_set - Add and element with the key/value to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: 1(Success) 0 (Failure)
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	unsigned long int size;
-	hash_node_t *actual_node;
-	hash_node_t *start;
+	hash_node_t *element = NULL;
+	unsigned long int index = 0;
 
-	if (ht == NULL)
+	if (!ht || !key || !(*key) || !value)
 		return (0);
 
-	size = ht->size;
-	index = key_index((const unsigned char *) key, size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-	actual_node = ht->array[index];
-
-	if (actual_node == NULL)
-	{
-		ht->array[index] = assign_values(key, value);
-		if (ht->array[index] == NULL)
-			return (0);
+	if (ht->array[index] && !(match_element(key, ht->array[index], value)))
 		return (1);
-	}
 
-	start = actual_node;
-
-	while (actual_node != NULL)
-	{
-		if (strcmp(actual_node->key, key) == 0)
-		{
-			free(actual_node->value);
-			actual_node->value = strdup(value);
-			return (1);
-		}
-		actual_node = actual_node->next;
-	}
-
-	ht->array[index] = assign_values(key, value);
-	if (ht->array[index] == NULL)
+	element = malloc(sizeof(hash_node_t));
+	if (!element)
 		return (0);
-	(ht->array[index])->next = start;
+	element->key = strdup(key);
+	element->value = strdup(value);
+	if (!(element->key) || !(element->value))
+	{
+		if (element->key)
+			free(element->key);
+		free(element);
+		return (0);
+	}
+	element->next = ht->array[index];
+	ht->array[index] = element;
 
 	return (1);
 }
-
 /**
- * assign_values - creates node
- *
- * @key: Key
- * @value: Value
- *
- * Return: Pointer to the new node or NULL if failure
-*/
-
-hash_node_t *assign_values(const char *key, const char *value)
+ * match_element - Check if a key value is already in a bucket.
+ * @key: key
+ * @array: bucket
+ * @value: node value to update
+ * Return: 1(No match Found) 0 (Match Found)
+ */
+int match_element(const char *key, hash_node_t *array, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *match = array;
 
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	return (new_node);
+	while (match)
+	{
+		if (!(strcmp(match->key, key)))
+		{
+			free(match->value);
+			match->value = strdup(value);
+			return (0);
+		}
+		match = match->next;
+	}
+	return (1);
 }
