1a471dbf622db3013ade4f01dcb5159f5f0fca66
Jamie897 <jamiedanielle456@gmail.com>
2023-11-22 19:04:37 -0600

    Fixing Syntax Errors

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,22 +2,17 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-int linear_search(int *array, size_t size, int value); {
+int linear_search(int *array, size_t size, int value) {
+    size_t i;
 
+    if (array == NULL)
+        return -1;
 
-	size_t i;
+    for (i = 0; i < size; i++) {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return i;
+    }
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-
-	return (-1);
+    return -1;
 }
-}
-
